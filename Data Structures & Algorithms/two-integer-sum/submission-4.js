49a4a2e9b02fcb2d4f41fa60ae0c2205187cdbf1
Andrey Dodonov <aadodonov@gmail.com>
2026-03-31 11:39:27 +0100

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let sorted = new Array(nums.length);
+        for (let i=0; i<nums.length; i++) {
+            sorted[i] = nums[i];
+        }
+        sorted.sort((a,b)=>(a-b));
+        let l = 0;
+        let r = sorted.length - 1;
+        let res = []
+        while (l<r) {
+            const sum = sorted[l] + sorted[r];
+            if (sum < target) {
+                l++;
+            } else if (sum > target) {
+                r--;
+            } else {
+                break;
+            }
+        }
+        res[0] = nums.indexOf(sorted[l]);
+        res[1] = nums.lastIndexOf(sorted[r]);
+        return res;
+    }
+}
