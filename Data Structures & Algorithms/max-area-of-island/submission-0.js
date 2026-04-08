112c89f7d5c1987c0549e7422ef3179d8f6e1b71
Andrey Dodonov <aadodonov@gmail.com>
2026-04-08 11:39:16 +0100

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    maxAreaOfIsland(grid) {
+        const ROWS = grid.length;
+        const COLS = grid[0].length;
+        function flood(r,c) {
+            if (r<0 || c<0 || r>=ROWS || c>=COLS) return 0;
+            if (0 === grid[r][c]) return 0;
+            grid[r][c] = 0;
+            let area = 1;
+            area += flood(r+1,c);
+            area += flood(r-1,c);
+            area += flood(r,c+1);
+            area += flood(r,c-1);
+            return area;
+        }
+        let maxArea = 0;
+        for (let r=0;r<ROWS;r++) {
+            for (let c=0;c<COLS;c++) {
+                maxArea = Math.max(maxArea,flood(r,c));
+            }
+        }
+        return maxArea;
+    }
+}
