7f43294be6227e5a8575c035631fd76b8cf87b75
Andrey Dodonov <aadodonov@gmail.com>
2026-04-14 11:29:12 +0100

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.js
@@ -0,0 +1,31 @@
+/**
+ * // Definition for a Node.
+ * class Node {
+ *     constructor(val = 0, neighbors = []) {
+ *       this.val = val;
+ *       this.neighbors = neighbors;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Node} node
+     * @return {Node}
+     */
+    cloneGraph(node) {
+        if (!node) return null;
+        const copiesArr = new Array(101).fill(null);
+        // copiesArr[node.val] = new Node(node.val, Array(node.neighbors.length));
+        function build(crt) {
+            if (copiesArr[crt.val]) return copiesArr[crt.val];
+            const copied = new Node(crt.val, Array(crt.neighbors.length));
+            copiesArr[crt.val] = copied;
+            for (let i=0;i<crt.neighbors.length;i++) {
+                copied.neighbors[i] = build(crt.neighbors[i]);
+            }
+            return copied;
+        }
+        return build(node);
+    }
+}
