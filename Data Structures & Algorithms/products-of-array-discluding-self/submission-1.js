67ef0f02a22be72571b6930e1a99437851683607
Andrey Dodonov <aadodonov@gmail.com>
2026-04-15 17:22:58 +0100

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        // obvious solution - calculate value where all are multiplied, and then divide
+        let mult = 1;
+        let nullCnt = 0;
+        let nullPos = -1;
+        for (let i=0;i<nums.length;i++) {
+            if (nums[i] !== 0) { 
+                mult *= nums[i];
+            } else {
+                nullCnt++;
+                nullPos = i;
+                if (nullCnt > 1) break;                    
+            }
+        }
+        if (nullCnt === 0) {
+            for (let i=0;i<nums.length;i++) {
+                nums[i] = mult / nums[i];
+            }
+        } else if (nullCnt === 1) {
+            for (let i=0;i<nums.length;i++) {
+                nums[i] = 0;
+            }
+            nums[nullPos] = mult;
+        } else {
+            for (let i=0;i<nums.length;i++) {
+                nums[i] = 0;
+            }
+        }
+        return nums;
+    }
+}
